e9444c8c30b7de343bfc4817c9a5643963943674
Shayan <ShayanSadeghi1996@gmail.com>
2022-11-29 19:26:22 +0330

    fix a problem on nav items

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,26 +6,18 @@ export default function Header() {
   return (
     <nav className={styles.container}>
       <ul>
-        <li className={styles.item}>
-          <Link to="/" className={styles.link}>
-            Home
-          </Link>
-        </li>
-        <li className={styles.item}>
-          <a href={cvFile} target="_blank" className={styles.link}>
-            CV
-          </a>
-        </li>
-        <li className={styles.item}>
-          <Link to="/papers" className={styles.link}>
-            Papers
-          </Link>
-        </li>
-        <li className={styles.item}>
-          <Link to="/awards" className={styles.link}>
-            Awards
-          </Link>
-        </li>
+        <Link to="/" className={styles.link}>
+          <li className={styles.item}>Home</li>
+        </Link>
+        <a href={cvFile} target="_blank" className={styles.link}>
+          <li className={styles.item}>CV</li>
+        </a>
+        <Link to="/papers" className={styles.link}>
+          <li className={styles.item}>Papers</li>
+        </Link>
+        <Link to="/awards" className={styles.link}>
+          <li className={styles.item}>Awards</li>
+        </Link>
       </ul>
     </nav>
   );
